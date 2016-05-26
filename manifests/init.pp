971dfbb54d0ffc7a20758e6af4af9b2ea11727e5
vincent <vincent-octo@users.noreply.github.com>
2016-05-26 16:22:05 +0200

    centos: use CC yum repository instead of wget

--- manifests/init.pp
+++ manifests/init.pp
@@ -36,8 +36,6 @@ class synergy (
   $keystone_admin_password,
 ){
   # Packages to download and install
-  $rdo_rpm = 'https://repos.fedorapeople.org/repos/openstack/openstack-liberty/rdo-release-liberty-3.noarch.rpm'
-  $synergy_service_rpm = 'https://github.com/indigo-dc/synergy-service/releases/download/v0.2/python-synergy-service-0.2-2.el7.centos.noarch.rpm'
   $synergy_service_deb = 'https://github.com/indigo-dc/synergy-service/releases/download/v0.2/python-synergy-service_0.2-1_all.deb'
 
   # Check which OS we are running on
@@ -52,29 +50,17 @@ class synergy (
     $os_version = undef
   }
 
-  package { 'wget':
-    ensure => present,
-  }
-
   # Install Synergy package and dependencies
-  # TODO better way to store this intermediate package files
   if $os_name == 'CentOS' and $os_version == '7' {
-    exec {'wget rdo.rpm':
-      command => "/usr/bin/wget -q -O /tmp/rdo.rpm $rdo_rpm",
-      creates => '/tmp/rdo.rpm',
-      require => Package['wget'],
+    yumrepo { 'cc-vendor':
+      descr    => 'Vendor repository for Synergy',
+      enabled  => 1,
+      baseurl  => "http://ccrepoli.in2p3.fr/linux/el/${os_version}x/${::architecture}/vendor",
+      gpgcheck => 0,
     }
 
-    file { '/tmp/rdo.rpm':
-      ensure  => present,
-      require => Exec['wget rdo.rpm'],
-    }
-
-    package { 'rdo-release':
-      provider => 'rpm',
-      ensure   => present,
-      source   => '/tmp/rdo.rpm',
-      require  => File['/tmp/rdo.rpm'],
+    package { 'centos-release-openstack-liberty':
+      ensure => latest,
     }
 
     package { 'python-dateutil':
@@ -82,62 +68,38 @@ class synergy (
     }
 
     package { 'python-eventlet':
-      ensure => present,
-    }
-
-    package { 'python-pip':
-      ensure => present,
-    }
-
-    # Fix puppet not being able to discover pip on *EL >= 6
-    # https://tickets.puppetlabs.com/browse/PUP-4997
-    file { '/usr/bin/pip-python':
-      ensure  => 'link',
-      target  => '/usr/bin/pip',
-      require => Package['python-pip'],
+      ensure  => present,
+      require => Package['centos-release-openstack-liberty'],
     }
   
-    package { 'oslo.config':
-      ensure   => present,
-      provider => 'pip',
-      require  => Package['python-pip'],
+    package { 'python2-oslo-config':
+      ensure  => present,
+      require => Package['centos-release-openstack-liberty'],
     }
 
     package { 'python-oslo-log':
       ensure  => present,
-      require => Package['rdo-release'],
+      require => Package['centos-release-openstack-liberty'],
     }
 
     package { 'python-oslo-messaging':
       ensure  => present,
-      require => Package['rdo-release'],
-    }
-
-    exec {'wget python-synergy-service.rpm':
-      command => "/usr/bin/wget -q -O /tmp/python-synergy-service.rpm $synergy_service_rpm",
-      creates => '/tmp/python-synergy-service.rpm',
-      require => Package['wget'],
-    }
-
-    file { '/tmp/python-synergy-service.rpm':
-      ensure  => present,
-      require => Exec['wget python-synergy-service.rpm'],
+      require => Package['centos-release-openstack-liberty'],
     }
 
     package { 'python-synergy-service':
-      provider => 'rpm',
       ensure   => present,
-      source   => '/tmp/python-synergy-service.rpm',
       require  => [
-        Package['python-dateutil'],
-        Package['python-eventlet'],
-        Package['oslo.config'],
-        Package['python-oslo-log'],
-        Package['python-oslo-messaging'],
-        File['/tmp/python-synergy-service.rpm'] ],
+        Package['centos-release-openstack-liberty'],
+        Yumrepo['cc-vendor'] ],
     }
   }
+  # TODO better way to store the intermediate package files
   elsif $os_name == 'Ubuntu' and $os_version == '14.04' {
+    package { 'wget':
+      ensure => present,
+    }
+
     exec { 'wget python-synergy-service.deb':
       command => "/usr/bin/wget -q -O /tmp/python-synergy-service.deb $synergy_service_deb",
       creates => '/tmp/python-synergy-service.deb',
